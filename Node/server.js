a012ad80784f0058abfb756b952e9c478e31cf5c
reddyvp08 <53430523+reddyvp08@users.noreply.github.com>
2023-01-13 22:38:53 +0530

    Delete Node directory

--- Node/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const fs =require('fs');
-app.get('/',function(req,resp){
-    resp.sendFile(path.join(__dirname+'/index.html'));
-});
-app.listen(3000);
-console.log("Server is started");
